8802c044b8725be5b4e92a5223fdeb1573f39f1a
Huizerd <jessehagenaars@gmail.com>
2019-10-16 16:07:51 +0200

    New network plotting

--- evolutionary/visualize/vis_network.py
+++ evolutionary/visualize/vis_network.py
@@ -1,10 +1,8 @@
 from collections import OrderedDict
 
 import torch
-import torch.nn as nn
+import numpy as np
 import matplotlib.pyplot as plt
-
-from pysnn.connection import Linear
 
 from evolutionary.network.ann import ANN
 from evolutionary.network.snn import SNN
@@ -21,32 +19,58 @@ def vis_network(config, parameters, debug=False, no_plot=False):
 
     network.load_state_dict(torch.load(parameters))
 
-    # Collect weights based on module being a connection or not
-    weights = OrderedDict(
-        [
-            (name, child.weight.T.detach().clone())
-            for name, child in network.named_children()
-            if isinstance(child, Linear) or isinstance(child, nn.Linear)
-        ]
-    )
-    w_min = min([w.min() for w in weights.values()])
-    w_max = max([w.max() for w in weights.values()])
-
-    # Build weight figure
-    fig_w, axs_w = plt.subplots(1, len(weights))
-    for ax, (name, weight) in zip(axs_w, weights.items()):
-        im = ax.imshow(weight.numpy(), cmap="plasma", vmin=w_min, vmax=w_max)
-        ax.set_title(name)
-        ax.set_xlabel("postsynaptic neuron id")
-        ax.set_ylabel("presynaptic neuron id")
-        fig_w.colorbar(im, ax=ax, orientation="vertical", fraction=0.1)
-
-    fig_w.tight_layout()
-
-    if not debug:
-        fig_w.savefig(
-            f"{config['log location']}weights+{'_'.join(config['individual id'])}.png"
+    # Go over all genes and create separate figures
+    # Note that weights/delays belong to connections/layers, while others belong to neurons
+    for gene in config["evo"]["genes"]:
+        params = OrderedDict(
+            [
+                (name, getattr(child, gene).detach().clone())
+                for name, child in network.named_children()
+                if hasattr(child, gene)
+            ]
         )
+        param_min = min([p.min().item() for p in params.values()])
+        param_max = max([p.max().item() for p in params.values()])
+
+        # Build figure
+        fig, axs = plt.subplots(2, len(params))
+        for i, (name, param) in zip(range(axs.shape[1]), params.items()):
+            # Colored value plots
+            if gene == "weight":
+                im = axs[0, i].imshow(
+                    param.T.numpy(), cmap="plasma", vmin=param_min, vmax=param_max
+                )
+                axs[0, i].set_xlabel("post neuron id")
+                axs[0, i].set_ylabel("pre neuron id")
+            else:
+                im = axs[0, i].imshow(
+                    param.numpy(), cmap="plasma", vmin=param_min, vmax=param_max
+                )
+            axs[0, i].set_title(f"{gene}: {name}")
+            fig.colorbar(im, ax=axs[0, i], orientation="vertical", fraction=0.1)
+            axs[0, i].tick_params(
+                axis="both",
+                which="both",
+                bottom=False,
+                top=False,
+                left=False,
+                right=False,
+                labelleft=False,
+                labelbottom=False,
+            )
+
+            # Histograms
+            axs[1, i].hist(param.view(-1).numpy(), range=(param_min, param_max))
+            axs[1, i].set_xlabel(f"{gene} value")
+            axs[1, i].set_ylabel("count")
+            axs[1, i].grid()
+
+        fig.tight_layout()
+
+        if not debug:
+            fig.savefig(
+                f"{config['log location']}{gene}+{'_'.join(config['individual id'])}.png"
+            )
 
     if not no_plot:
         plt.show()
