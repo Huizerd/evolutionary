69fa02856b600ba9be222a5c5651a1a5f975c6ce
Huizerd <jessehagenaars@gmail.com>
2019-10-17 09:54:38 +0200

    Noplot

--- run_baselines.sh
+++ run_baselines.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-python main.py --config configs/snn+hover.yaml --tags @nonothing @signed+offset @equaldynamics
-python main.py --config configs/snn+hover2.yaml --tags @nonothing @signed+offset @equaldynamics @doubleneuron
-python main.py --config configs/snn+landing.yaml --tags @landing @equaldynamics
-python main.py --config configs/snn+landing2.yaml --tags @landing @equaldynamics @doubleneuron
-python main.py --config configs/ann+hover.yaml --tags @nonothing @signed+offset
-python main.py --config configs/ann+landing.yaml --tags @landing
+python main.py --noplot --config configs/snn+hover.yaml --tags @nonothing @signed+offset @equaldynamics
+python main.py --noplot --config configs/snn+hover2.yaml --tags @nonothing @signed+offset @equaldynamics @doubleneuron
+python main.py --noplot --config configs/snn+landing.yaml --tags @landing @equaldynamics
+python main.py --noplot --config configs/snn+landing2.yaml --tags @landing @equaldynamics @doubleneuron
+python main.py --noplot --config configs/ann+hover.yaml --tags @nonothing @signed+offset
+python main.py --noplot --config configs/ann+landing.yaml --tags @landing
--- run_baselines2.sh
+++ run_baselines2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python main.py --config configs/snn+hover2.yaml --tags @nonothing @signed+offset @equaldynamics @doubleneuron
-python main.py --config configs/snn+landing2.yaml --tags @landing @equaldynamics @doubleneuron
+python main.py --noplot --config configs/snn+hover2.yaml --tags @nonothing @signed+offset @equaldynamics @doubleneuron
+python main.py --noplot --config configs/snn+landing2.yaml --tags @landing @equaldynamics @doubleneuron
