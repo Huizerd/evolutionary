4439871e60ca96623d0071bf751bb7ddd0a60210
Huizerd <jessehagenaars@gmail.com>
2019-10-24 12:27:50 +0200

    Verbose

--- run_baselines.sh
+++ run_baselines.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-python main.py --noplot --config configs/snn+landing1a.yaml --tags @snn @landing @40
-python main.py --noplot --config configs/snn+landing1b.yaml --tags @snn @landing @nowind
-python main.py --noplot --config configs/snn+landing1c.yaml --tags @snn @landing @1out
-python main.py --noplot --config configs/snn+landing2a.yaml --tags @snn @landing @inc
-python main.py --noplot --config configs/snn+landing3a.yaml --tags @snn @landing @1in1out
-python main.py --noplot --config configs/snn+landing4a.yaml --tags @snn @landing @weighted
+python main.py --verbose 1 --config configs/snn+landing1a.yaml --tags @snn @landing @40
+python main.py --verbose 1 --config configs/snn+landing1b.yaml --tags @snn @landing @nowind
+python main.py --verbose 1 --config configs/snn+landing1c.yaml --tags @snn @landing @1out
+python main.py --verbose 1 --config configs/snn+landing2a.yaml --tags @snn @landing @inc
+python main.py --verbose 1 --config configs/snn+landing3a.yaml --tags @snn @landing @1in1out
+python main.py --verbose 1 --config configs/snn+landing4a.yaml --tags @snn @landing @weighted
