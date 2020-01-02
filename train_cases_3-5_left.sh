8735c154debfc54f1f3f283b5a3b9538f416de7f
Huizerd <jessehagenaars@gmail.com>
2020-01-02 23:16:19 +0100

    Cases 3 4 5 analysis

---
+++ train_cases_3-5_left.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# Left: run parallel with right (since we only use half of the cores)
+# Case 3
+python main.py --config configs/case3a.yaml --tags case3a left
+python main.py --config configs/case3b.yaml --tags case3b left
+
+# Case 4
+python main.py --config configs/case4a.yaml --tags case4a left
+python main.py --config configs/case4b.yaml --tags case4b left
+python main.py --config configs/case4c.yaml --tags case4c left
+
+# Case 5
+python main.py --config configs/case5a.yaml --tags case5a left
+python main.py --config configs/case5b.yaml --tags case5b left
+python main.py --config configs/case5c.yaml --tags case5c left
---
+++ train_cases_3-5_right.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# Right: run parallel with left (since we only use half of the cores)
+# Case 3
+python main.py --config configs/case3a.yaml --tags case3a right
+python main.py --config configs/case3b.yaml --tags case3b right
+
+# Case 4
+python main.py --config configs/case4a.yaml --tags case4a right
+python main.py --config configs/case4b.yaml --tags case4b right
+python main.py --config configs/case4c.yaml --tags case4c right
+
+# Case 5
+python main.py --config configs/case5a.yaml --tags case5a right
+python main.py --config configs/case5b.yaml --tags case5b right
+python main.py --config configs/case5c.yaml --tags case5c right
