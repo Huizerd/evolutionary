9a17bf2e97607d1204147e830fc7cb7133190ba5
Huizerd <jessehagenaars@gmail.com>
2020-08-29 22:01:00 +0200

    Training file

--- train.sh
+++ train.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
 
-python main.py --config configs/defaults.yaml --tags defaults Dblind
-python main.py --config configs/defaults.yaml --tags defaults Dblind
-
-python main.py --config configs/2x5neurons.yaml --tags 2x5neurons Dblind
-python main.py --config configs/2x5neurons.yaml --tags 2x5neurons Dblind
+python main.py --config configs/spike-encoding.yaml --tags spike-enc
+python main.py --config configs/spike-encoding.yaml --tags spike-enc
