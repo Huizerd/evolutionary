d1a44a462af2951b4fc3543b67d3ad8d8b061561
Huizerd <15855769+Huizerd@users.noreply.github.com>
2020-09-18 09:21:31 +0200

    New plots

---
+++ extra/plot_transient_neurons.py
@@ -0,0 +1,155 @@
+import argparse
+import yaml
+import os
+import shutil
+
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rcParams["lines.linewidth"] = 0.8
+
+from evolutionary.network.snn import TwoLayerSNN
+from evolutionary.utils.constructors import build_network, build_environment
+from evolutionary.utils.utils import randomize_env
+
+
+def plot_transient_neurons(folder, parameters):
+    individual_id = "_".join(
+        [s.replace(".net", "") for s in parameters.split("/")[-2:]]
+    )
+    save_folder = folder + f"/transient+neurons+{individual_id}"
+    suffix = 0
+    while os.path.exists(f"{save_folder}+{str(suffix)}/"):
+        suffix += 1
+    save_folder += f"+{str(suffix)}/"
+    os.makedirs(save_folder)
+
+    # Load config
+    with open(folder + "/config.yaml", "r") as cf:
+        config = yaml.full_load(cf)
+
+    # Build environment
+    env = build_environment(config)
+
+    # Load network
+    network = build_network(config)
+    network.load_state_dict(torch.load(parameters))
+    network.reset_state()
+
+    # 100 runs
+    time_list = []
+    obs_list = []
+    obs_error_list = []
+    neuron_spike_list = []
+
+    for i in range(10):
+        env = randomize_env(env, config)
+        network.reset_state()
+        obs = env.reset(h0=config["env"]["h0"][1])
+        done = False
+
+        # For plotting
+        time = []
+        observations = []
+        obs_errors = []
+        neuron_spikes = []
+
+        while not done:
+            # Step the environment
+            obs = torch.from_numpy(obs)
+            action = network.forward(obs.clone().view(1, 1, -1))
+            action = action.numpy()
+
+            if env.t >= env.settle:
+                time.append(env.t)
+                observations.append(obs.numpy().copy())
+                obs_errors.append(
+                    obs.numpy().copy() - np.array([config["evo"]["D setpoint"], 0.0])
+                )
+                if isinstance(network, TwoLayerSNN):
+                    in_spikes = network.input.view(-1).numpy().copy()
+                    hid_spikes = network.neuron1.spikes.float().view(-1).numpy().copy()
+                    out_spikes = network.out_spikes.float().view(-1).numpy().copy()
+                    neuron_spikes.append(
+                        np.concatenate((in_spikes, hid_spikes, out_spikes))
+                    )
+                else:
+                    raise ValueError(f"Incompatible network type specified")
+
+            obs, _, done, _ = env.step(action)
+
+        time_list.append(time)
+        obs_list.append(observations)
+        obs_error_list.append(obs_errors)
+        neuron_spike_list.append(neuron_spikes)
+
+    # Activity vs D error
+    # Activity vs time
+    fig1, ax1 = plt.subplots(
+        max(config["net"]["layer sizes"]),
+        len(config["net"]["layer sizes"]),
+        sharex=True,
+        sharey=True,
+        figsize=(12, 18),
+    )
+    fig2, ax2 = plt.subplots(
+        max(config["net"]["layer sizes"]),
+        len(config["net"]["layer sizes"]),
+        sharex=True,
+        sharey=True,
+        figsize=(12, 18),
+    )
+    n_plots_oe = [[] for _ in range(sum(config["net"]["layer sizes"]))]
+    n_plots_act = [[] for _ in range(sum(config["net"]["layer sizes"]))]
+
+    for tim, ob, ober, neur in zip(
+        time_list, obs_list, obs_error_list, neuron_spike_list
+    ):
+        for t, o, oe, n in zip(tim, ob, ober, neur):
+            for i in range(len(n_plots_oe)):
+                n_plots_oe[i].append(oe[0])
+                n_plots_act[i].append(n[i])
+
+    k = 0
+    for i in range(len(config["net"]["layer sizes"])):
+        for j in range(config["net"]["layer sizes"][i]):
+            sort_idx = np.argsort(n_plots_oe[k])
+            ma = (
+                pd.Series(np.array(n_plots_act[k])[sort_idx])
+                .rolling(window=40, min_periods=1)
+                .mean()
+                .values
+            )
+            ax1[j, i].plot(np.array(n_plots_oe[k])[sort_idx], ma)
+            k += 1
+
+    k = 0
+    for i in range(len(config["net"]["layer sizes"])):
+        for j in range(config["net"]["layer sizes"][i]):
+            for tim, neur in zip(time_list, neuron_spike_list):
+                ma = (
+                    pd.Series(np.array(neur)[:, k])
+                    .rolling(window=40, min_periods=1)
+                    .mean()
+                    .values
+                )
+                ax2[j, i].plot(tim, ma)
+            k += 1
+
+    fig1.tight_layout()
+    fig2.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", type=str, required=True)
+    parser.add_argument("--parameters", type=str, required=True)
+    args = vars(parser.parse_args())
+
+    # Call
+    plot_transient_neurons(args["folder"], args["parameters"])
---
+++ extra/plot_weights.py
@@ -0,0 +1,91 @@
+import argparse
+import os
+import yaml
+
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pysnn.neuron import AdaptiveLIFNeuron
+
+from evolutionary.network.snn import TwoLayerSNN, ThreeLayerSNN
+from evolutionary.utils.constructors import build_network
+
+
+def plot_weights(folder, parameters):
+    individual_id = "_".join(
+        [s.replace(".net", "") for s in parameters.split("/")[-2:]]
+    )
+    save_folder = folder + f"/weights+{individual_id}/"
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    # Load config
+    with open(folder + "/config.yaml", "r") as f:
+        config = yaml.full_load(f)
+
+    # Build network
+    network = build_network(config)
+    # Load network parameters
+    network.load_state_dict(torch.load(parameters))
+
+    # Two-layer
+    if isinstance(network, TwoLayerSNN):
+        # Create figure
+        fig, ax = plt.subplots(2, 2)
+
+        # In -> hid
+        # Square
+        im1 = ax[0, 0].imshow(
+            network.fc1.weight.data.numpy(), vmin=-256, vmax=254, cmap="viridis"
+        )
+        ax[0, 0].set_xlabel("pre id")
+        ax[0, 0].set_ylabel("post id")
+        ax[0, 0].set_title("in -> hid")
+        cbar1 = fig.colorbar(im1, ax=ax[0, 0])
+        cbar1.set_label("weight")
+        # Histogram
+        ax[0, 1].hist(
+            network.fc1.weight.data.view(-1).numpy(),
+            range=[-256, 254],
+            bins=20,
+            edgecolor="black",
+        )
+        ax[0, 1].set_xlabel("weight")
+        ax[0, 1].set_ylabel("count")
+        ax[0, 1].set_title("in -> hid")
+
+        # Hid -> out
+        # Square
+        im2 = ax[1, 0].imshow(
+            network.fc2.weight.data.numpy(), vmin=-256, vmax=254, cmap="viridis"
+        )
+        ax[1, 0].set_xlabel("pre id")
+        ax[1, 0].set_ylabel("post id")
+        ax[1, 0].set_title("hid -> out")
+        cbar2 = fig.colorbar(im2, ax=ax[1, 0])
+        cbar2.set_label("weight")
+        # Histogram
+        ax[1, 1].hist(
+            network.fc2.weight.data.view(-1).numpy(),
+            range=[-256, 254],
+            bins=20,
+            edgecolor="black",
+        )
+        ax[1, 1].set_xlabel("weight")
+        ax[1, 1].set_ylabel("count")
+        ax[1, 1].set_title("hid -> out")
+
+        fig.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", type=str, required=True)
+    parser.add_argument("--parameters", type=str, required=True)
+    args = vars(parser.parse_args())
+
+    # Call
+    plot_weights(args["folder"], args["parameters"])
